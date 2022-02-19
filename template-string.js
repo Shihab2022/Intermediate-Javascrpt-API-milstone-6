9610514c143a41ba6e0a93ab0dfd8f208d688fbd
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-19 21:34:29 +0600

    template string

---
+++ template-string.js
@@ -0,0 +1,37 @@
+/* //jodi [' '] ay vaby king ba dubbole qutation [" "] babohar kori taholay line ay jodi enter diay jodi notun line ay ni taholay ay ti kaj korbay na tai amakay temoalte string babohr akorty hobay ///
+
+
+abar jodi single ba dubole qutation use kori tar por enter diay jodi line bekerk dia y jodi porar line ay niay jai tar poro jod i console log kori taholay sob kisu akl lin eay day kabay ar jonno amra backtik use korbo  */
+
+/* const name = 'my name is md shihab uddin . I read in Rajshahi University.'
+' I want to be a web-developer'
+console.log(name) */
+
+// use backtik
+
+//backtik ar moddy jay vaby liko output ay say vaby dakaby 
+
+/* const detiles = `my name is md shihab uddin . I read in Rajshahi University
+
+I want to become a web-developer.
+
+in 2023.`
+console.log(detiles) */
+
+
+/* const friends = ['abul', 'fabul', 'kabul ', 'dabul'];
+const count = 5; */
+/* // const old = `<h3 class="friend-name">Friend - '+ ${count }+'</h3>`;
+// console.log(old); */
+
+/* // const newArray = `<h3 class="friend-name">Friend + ${friends.length}</h3>`;
+// console.log(newArray);
+ */
+// amra backtik ar moddya akdik conditio add korty pari k
+
+
+/* const first = 'shihab';
+const last = 'uddin'
+const fullname = `This person name is : ${first} ${last} . 
+He reads in honuse 2nd year . and  ${friends.length*500} .he livs in chapainowabgonj`;
+console.log(fullname) */
