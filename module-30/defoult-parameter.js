ea94f30747bf0e16ad0427f564d5c707b9d9e4ab
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-19 20:42:33 +0600

    defoult parameter

---
+++ module-30/defoult-parameter.js
@@ -0,0 +1,14 @@
+// function add(num1, num2) {
+//     const total = num1 + num2;
+//     return total;
+// }
+// const numbers = add(29, 40);
+// console.log(numbers)
+
+//ay khanay amra const total = num1 + num2; na diay sora sori return koray diaty pari 
+function add(num1, num2) {
+
+    return num1 + num2;
+}
+const numbers = add(29, 40);
+console.log(numbers)
