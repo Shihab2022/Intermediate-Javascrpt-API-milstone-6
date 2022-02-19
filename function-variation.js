662e09261ccff6ad3c0cf1c325e5fba8596f99f7
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-19 21:48:43 +0600

    function varition

---
+++ function-variation.js
@@ -0,0 +1,36 @@
+//amra function kay bivinno vabay liktay pari tar moodday koi ak ta nisahy dou  aholo
+
+//function decleration [ normal function]
+
+/* function sum(num1, num2) {
+    return num1 + num2;
+}
+const numbers = sum(29, 40);
+console.log(numbers) */
+
+
+// function expression
+// ay ti akti vari able ar  maddo may function  ar name doua  hoay say 
+
+/* const sum = function sum(num1, num2) { 
+    return num1 + num2;
+}
+const numbers = sum(29, 40);
+console.log(numbers) */
+
+
+//anonymous function expression
+//atri son kisu agar function ar mot o sudu parokko holo jay ay functio ar kono name ditay hoy na
+
+/* const sum = function(num1, num2) {
+    return num1 + num2;
+}
+const numbers = sum(29, 40);
+console.log(numbers) */
+
+
+
+/******************************** *  Arrow Functions ********************************/
+const sum = (num1, num2) => num1 + num2;
+const numbers = sum(29, 40);
+console.log(numbers)
