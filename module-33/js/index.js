13923c952d94502964051912d080d4a84cb53343
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-22 22:00:04 +0600

    add more jeson file

--- module-33/js/index.js
+++ module-33/js/index.js
@@ -1,14 +1,20 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => addEvent(data))
+function addEvent() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => addFunction(data))
+
+}
 
 
-function addEvent(data) {
-    // const ul = document.getElementById('user');
+function addFunction(data) {
+    const ul = document.getElementById('user');
 
     for (const users of data) {
-        console.log(users)
+        const li = document.createElement('li');
+        // li.innerText = users.name;  //for name
+        // li.innerText = users.email; //for email
+        li.innerText = `Client Name : ${users.name}  and    Your email address : ${users.email}`; //dainamically add 
+        ul.appendChild(li)
     }
-
-
 }
