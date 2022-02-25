be66338b0cc928ecbe7bd29a1921585a952236b2
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-25 07:39:31 +0600

    add more for sports db

--- module-34-5/sports-db.js
+++ module-34-5/sports-db.js
@@ -13,19 +13,27 @@ const addName = (datas) => {
         const mainContainer = document.getElementById('main-container')
         const div = document.createElement('div')
         div.classList.add('style-card')
-        div.innerHTML = `
-<img style="height: 200px;border-radius: 20px;" width="100%" src="${data.strSportThumb}">
-<img  src="${data.strSportIconGreen}">
 
-<p>${data.strFormat}</p>
-<p>${data.strSportIconGreen}</p>
-<p>${data.strFormat}</p>
+        div.innerHTML = `
+    
+<img style="height: 200px;border-radius: 20px;" width="100%" src="${data.strSportThumb}">
+ 
+<div class="footer">
+<div> <h2 style="color:red;">${data.strSport}</h2>
+<h4>${data.strFormat}</h4></div>
+<img  src="${data.strSportIconGreen}">
+ </div>
+<p>${data.strSportDescription.slice(0,300)}</p>
+
 `
-        console.log(data)
+            // console.log(data)
         mainContainer.appendChild(div)
 
     }
 
+}
+const click = () => {
+    console.log('hello')
 }
 
 { /* <img src="${data.strSportIconGreen}"></img> */ }
