0e24c0ae8f397ea44cea7f85315299e5c14781b1
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-26 17:32:11 +0600

    undefined null

---
+++ module-35/undefined-null.js
@@ -0,0 +1,46 @@
+//amra onk vaby output undefined paity pari 
+
+//1.variable value is not assigned 
+// let x;
+// console.log(x)
+
+// 2 . function but did not write return
+// function sum(x, y) {
+//     const result = x + y;
+// }
+// console.log(sum(10, 30))
+
+// 3. just return but did nnor return anything
+
+// function sum(x, y) {
+//     const result = x + y;
+//     return
+// }
+// console.log(sum(10, 30))
+
+// 4. parameter doesnot pass
+
+// function sum(x, y) {
+//     const result = x + y;
+//     return result;
+// }
+// console.log(sum(10))
+
+
+// 5. property does not exist in an object
+// const newObject= { name: 'shihab', id: '2010628126' }
+// console.log(newObject.profession)
+
+// 6. accessing array element out of range 
+
+// const newArray = [1, 3, 4, 6, 8, 99, 6]
+// console.log(newArray[10])
+
+
+/****defirent undefined and null*****/
+
+
+//undefined output amra onak karonary paity pari kintu amra null value hisaby jodi dia taholay out put hisaby amra null paity pari
+
+// const detiles = { name: 'shihab', id: '2010628126', profession: null }
+// console.log(detiles)
