73726a9f217d9d8f47a43c305b3db69800d5d2cd
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-26 16:56:16 +0600

    add type and premitive non premitive

---
+++ module-35/premitive-non-premitive.js
@@ -0,0 +1,16 @@
+//data type hoity say mot 7 prokar 
+// 1. number 2.string 3.boolean 4.undefined 5.null 6.object 7.symbol
+
+// 7 prokar type ar modday type gulo abar 2 prokar
+
+// 1 . primitive type : jar akti nirdisto man thakay takay premitive value bolay
+// 2 . non-primitive type : jar akti nirdisto man thakay na thakay non-primitive value
+
+
+//primitive type
+
+// 1. number 2.string 3.boolean 4.undefined 5.null 6.symbol
+
+
+// non-primitive type
+// 7 . object
---
+++ module-35/type-of.js
@@ -0,0 +1,9 @@
+const string = 'shihab'
+    // console.log(typeof number)
+const number = 9000
+    // console.log(typeof number)
+const type = '500'
+    // console.log(typeof type)
+
+const bulian = true
+console.log(typeof bulian)
