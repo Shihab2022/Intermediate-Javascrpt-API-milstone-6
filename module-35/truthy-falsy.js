21926c776a0b5d2aa7c549ed60caa9218ea9fdfc
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-26 17:14:35 +0600

    add truthy falsahy

---
+++ module-35/truthy-falsy.js
@@ -0,0 +1,34 @@
+// truthy values
+
+// const x = true  //true  bulian value
+// const x = 4  //sokol positive number without 0
+// const x = -1  //sokol negative number without 0
+// const x = 'shihab'  //sokol string
+// const x = ' ' //skati faka 
+// const x = '0'
+// const x = {} //jay kono faka array king ba  { name :'shihab' ,roll:2010628126} aytou truthy
+// const x = []  //jay kono object 
+
+/* if (x) {
+    console.log('variable is truthay')
+} else {
+    console.log('variable is falsey')
+} */
+
+
+// falsey valuse 
+
+// const x = false;  //false bolian value
+// const x = 0  //sudu number ar modday 0 falsahy value
+// const x = '' //empty string
+// const x = null; //primitive type null
+// const x = NaN  //jodi kono  falue  not a number monay number na hoy taholay sati number hisaby cheak korlay sayti NaN dakai say tai holo falsahy valua
+
+/* if (x) {
+    console.log('variable is truthay')
+} else {
+    console.log('variable is falsey')
+} */
+
+
+/********Any thing else that is not falshy will be truthy***********/
